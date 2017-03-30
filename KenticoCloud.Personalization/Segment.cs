8f2231cb1029641cbf18c55f80e2a40553d68fcc
Lukas Pirkl <pirkl.lukas@gmail.com>
2017-03-30 11:54:59 +0200

    ENG-1311 - Added method to get segments in which current user is.

---
+++ KenticoCloud.Personalization/Segment.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace KenticoCloud.Personalization
+{
+    /// <summary>
+    /// Represents segment.
+    /// </summary>
+    public class Segment
+    {
+        /// <summary>
+        /// Name of the segment.
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
