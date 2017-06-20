734f91f0634a60979e1236b773906a8735911b89
Richard Šefr <richard.sefr@gmail.com>
2017-06-20 15:44:52 +0200

    Rename SegmentsRespons file to SegmentsResponse

---
+++ KenticoCloud.Personalization/SegmentsResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace KenticoCloud.Personalization
+{
+    /// <summary>
+    /// Represents collection of segments.
+    /// </summary>
+    public class SegmentsResponse
+    {
+        /// <summary>
+        /// Collection of segments.
+        /// </summary>
+        [JsonProperty("segments")]
+        public Segment[] Segments { get; set; }
+    }
+}
